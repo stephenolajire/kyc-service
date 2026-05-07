3179e50d90d1ad3597042f5a4611bc69d9d37057
olajirestephen <horlharmighty2000@gmail.com>
2026-05-07 12:59:54 +0100

    added fix more

--- verification/apps.py
+++ verification/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class VerificationConfig(AppConfig):
     name = 'verification'
+
+    def ready(self):
+        from deepface import DeepFace
+        DeepFace.build_model("Facenet")
